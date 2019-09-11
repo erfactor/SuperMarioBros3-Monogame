6eafd864ed33aa3ef137736f16c4ce7694850385
r.chabasinsk <r.chabasinsk@samsung.com>
2019-09-11 08:51:46 +0200

    Minor cleanup in map view code
    Signed-off-by: r.chabasinsk <r.chabasinsk@samsung.com>

--- Mapa/Vertice.cs
+++ Mapa/Vertice.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-
-namespace SuperMarioBros
+﻿namespace SuperMarioBros
 {
     public class Vertex
     {
@@ -28,5 +20,5 @@ namespace SuperMarioBros
             else MainView.SwitchMode(action);
         }
     }
-    
+
 }
